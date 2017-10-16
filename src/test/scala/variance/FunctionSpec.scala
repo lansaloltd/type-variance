8f9dc046dd931f816c36707377a074a67e4b2138
lansaloltd <lansaloltd@gmail.com>
2017-10-16 18:05:13 +0100

    Added few comments

--- src/test/scala/variance/FunctionSpec.scala
+++ src/test/scala/variance/FunctionSpec.scala
@@ -1,5 +1,6 @@
 package variance
 
+import kingdom.animalia.Animal
 import kingdom.animalia.vertebrate.Vertebrate
 import kingdom.animalia.vertebrate.mammal.Mammal
 import kingdom.animalia.vertebrate.mammal.primate.Primate
@@ -62,9 +63,45 @@ class FunctionSpec extends FreeSpec with Matchers {
     }
   }
 
-  /*"Test 4: Variance as defined for Function, preserve : " - {
+  "Test 4: Variance and function composition : " - {
+    //f(x) compose g(x) -> f(g(x))
 
-  }*/
+    // We can clearly compose this way
+    val fromVertebrateToPrimate: Vertebrate => Primate = fromVertebrateToMammal andThen fromMammalToPrimate
+
+    // Because Function is contravariant on its argument, we CAN replace
+    // fromMammalToPrimate with a function that takes as a argument a parent of Mammal
+    // (in this example Vertebrate which is a supertype of Mammal)
+    val fromVertebrateToMammal1: Vertebrate => Mammal = fromVertebrateToMammal andThen fromVertebrateToMammal
+
+    // We can also take advantage that the Function type is covariant in its return type
+    // so if consider our first compose function: fromVertebrateToMammal andThen fromMammalToPrimate
+    // we CAN replace fromVertebrateToMammal with a function that returns a child of Mammal (Homo is
+    // indeed a supertype of Mammal)
+
+    val fromVertebrateToPrimate2: Vertebrate => Primate = fromVertebrateToHomo andThen fromMammalToPrimate
+
+    // But we CANNOT make the argument of fromMammalToPrimate covariant. If we use a child of Mammal
+    // (like the supertype Primate in this example) that WILL NOT compile because fromPrimateToPrimate
+    // is not defined for the whole Mammals' domain. In other worlds, if we pass a puff fish (which is
+    // a vertebrate) to fromVertebrateToMammal, this function might return a Rhinoceros (which is a mammal
+    // but NOT a primate). So, what is our function fromPrimateToPrimate supposed to do with a rhinoceros?
+
+    //fromVertebrateToMammal andThen fromPrimateToPrimate
+
+
+    // NOR we can make the return type contravariant for analogous considerations.
+    // Let's consider the initial composite function fromVertebrateToMammal andThen fromMammalToPrimate:
+    // If we try to make the return type of fromVertebrateToMammal contravariant, we have to pick up a parent
+    // of Mammal (for example Animal, which is a super type of Mammal) and replace fromVertebrateToMammal with
+    // fromVertebrateToAnimal, but the following WILL NOT compile. What about if I pass a puff fish to fromVertebrateToAnimal
+    // and this one returns a jellyfish, which is an animal but definitely not a mammal. fromMammalToPrimate is not defined
+    // for jellyfishes and would break
+
+    //fromVertebrateToAnimal andThen fromMammalToPrimate
+
+
+  }
 
 
 }
@@ -75,27 +112,31 @@ object Fixture {
 
   def calculateStringLength(s: String, f: String => Int): Int = f(s)
 
-  var fromMammalToPrimate: Function1[Mammal, Primate] = new Function1[Mammal, Primate] {
+  var fromMammalToPrimate: Mammal => Primate = new Function1[Mammal, Primate] {
     def apply(x: Mammal): Primate = new Primate()
   }
 
-  val fromVertebrateToHomo: Function1[Vertebrate, Homo] = new Function1[Vertebrate, Homo] {
+  val fromVertebrateToHomo: Vertebrate => Homo = new Function1[Vertebrate, Homo] {
     def apply(x: Vertebrate): Homo = new Homo()
   }
 
-  val fromVertebrateToMammal: Function1[Vertebrate, Mammal] = new Function1[Vertebrate, Mammal] {
+  val fromVertebrateToMammal: Vertebrate => Mammal = new Function1[Vertebrate, Mammal] {
     def apply(x: Vertebrate): Mammal = new Mammal()
   }
 
-  val fromPrimateToPrimate: Function1[Primate, Primate] = new Function1[Primate, Primate] {
+  val fromVertebrateToAnimal: Vertebrate => Animal = new Function1[Vertebrate, Animal] {
+    def apply(x: Vertebrate): Animal = new Animal()
+  }
+
+  val fromPrimateToPrimate: Primate => Primate = new Function1[Primate, Primate] {
     def apply(x: Primate): Primate = x
   }
 
-  var fromStringToInt: Function1[String, Int] = new Function1[String, Int] {
+  var fromStringToInt: String => Int = new Function1[String, Int] {
     def apply(x: String): Int = x.length
   }
 
-  var fromAnyRefToInt: Function1[AnyRef, Int] = new Function1[AnyRef, Int] {
+  var fromAnyRefToInt: AnyRef => Int = new Function1[AnyRef, Int] {
     def apply(x: AnyRef): Int = -5
   }
 
